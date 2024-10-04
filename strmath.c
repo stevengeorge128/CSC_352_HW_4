fe1e58cade14aed7bf5a44ec50b7291d96ebf069
stevengeorge128 <stevengeorge@arizona.edu>
2024-10-04 15:44:22 -0700

    Initial

---
+++ strmath.c
@@ -0,0 +1,16 @@
+/*
+* File: strmath.c* Author: Steven George
+* Purpose: Read in two strings of unknown length consisting of solely integers
+* perform the given operation on the strings (either addition or subtraction)
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int errorOccurred = 0;
+
+
+
+    return errorOccurred;
+}
