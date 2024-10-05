233bd4b7c86b42aceef364ec9dfd38bc855cbe95
stevengeorge128 <stevengeorge@arizona.edu>
2024-10-04 22:13:51 -0700

    done

--- strmath.c
+++ strmath.c
@@ -1,7 +1,8 @@
 /*
  * File: strmath.c* Author: Steven George
- * Purpose: Read in two strings of unknown length consisting of solely integers
- * perform the given operation on the strings (either addition or subtraction)
+ * Purpose: Read in two strings of unknown length consisting of solely integers and
+ * perform the given operation on the strings (either addition or subtraction). Features
+ * getline()
  */
 
 #include <stdio.h>
@@ -9,6 +10,11 @@
 #include <string.h>
 #include <ctype.h>
 
+/* checkReturnValue(int val, int returnIfValue, char *message) -- Checks 
+if the given int val is less than returnIfValue, erroring using 
+the given message if yes. Assumes int val, int returnIfValue, and character
+array message. Not returns.
+*/
 void checkReturnValue(int val, int returnIfValue, char *message)
 {
     if (val < returnIfValue)
@@ -18,6 +24,10 @@ void checkReturnValue(int val, int returnIfValue, char *message)
     }
 }
 
+/* newlineToNullTerm(char *operation) -- Takes a character array, removes the trailing
+newline and replaces it with the null terminator. Assumes character array pointer. No
+return value.
+*/
 void newlineToNullTerm(char *operation)
 {
     if (operation[strlen(operation) - 1] == '\n')
@@ -26,6 +36,10 @@ void newlineToNullTerm(char *operation)
     }
 }
 
+/* isArrayNumeric(char *str) -- Check if an array is numeric. Returns 1 if yes, false
+otherwise. Exits program with error upon receiving an empty string. Assumes char pointer
+input. Returns int. 
+*/
 int isArrayNumeric(char *str)
 {
     if (strcmp(str, "") == 0){
@@ -42,15 +56,11 @@ int isArrayNumeric(char *str)
     return 1;
 }
 
+/*removeLeadingZeros(char *str) -- Removes leading zeros from character array. Assumes
+character pointer input. Returns character array pointer
+*/
 char *removeLeadingZeros(char *str)
 {
-    // int len = strlen(str);
-    // int breakPoint = -1;
-    // for (int i = 0; i < len; i ++){
-    //     if (str[i] != '0'){
-    //         return str[i];
-    //     }
-    // }
     // While first character is zero and we haven't reached the null terminator
     while (*str == '0' && *(str + 1) != '\0')
     {
@@ -59,12 +69,13 @@ char *removeLeadingZeros(char *str)
     return str;
 }
 
+/* char *addLeadingZeros(char *arr, int newSize) -- Adds leading zeros to arr until 
+arr matches the size of newSize. Takes character arr of string to update the the new size.
+Assumes newSize is larger than arr size. Returns char array pointer. 
+*/
 char *addLeadingZeros(char *arr, int newSize)
 {
-    // printf("----------\n inside addLeadingZeros");
-    // printf("new size is %d\n", newSize);
-    // printf("arr len is %lu\n" , strlen(arr));
-    // printf("arr is %s\n", arr);
+    // Allocate memory and error check. 
     int oldArrayIndex = 0;
     char *newArr = malloc(sizeof(char) * newSize);
     if (newArr == NULL)
@@ -72,50 +83,52 @@ char *addLeadingZeros(char *arr, int newSize)
         fprintf(stderr, "Error: Out of memory");
         exit(1);
     }
+    // Calc zeros to add and return arr if none
     int zerosToAdd = newSize - strlen(arr);
     if (zerosToAdd == 0)
     {
         return arr;
     }
-    // printf("zerosToAdd is  %d\n", zerosToAdd);
+    // Else loop through newArr and add zeros, or switch to old array values.
     for (int i = 0; i < newSize; i++)
     {
-        //  printf("newArr is %s\n", newArr);
-        //  printf("i is %d\n", i);
         if (i < zerosToAdd)
         {
             newArr[i] = '0';
         }
         else
         {
-            //  printf("Inside the else statement where oldArrayIndex is %d\n", oldArrayIndex);
             newArr[i] = arr[oldArrayIndex];
             oldArrayIndex++;
         }
-        // printf("here\n");
     }
-    // printf("Leaving addLeadingZeros\n ----------\n ");
-
     return newArr;
 }
 
+/*char *subtractString(char *str1, char *str2) -- Major method for string subtraction. 
+Calculates the value by iterating through the length of the longer string and conducting
+subtraction on each corresponding index, starting with the rightmost, or least significant,
+digit. Borrows from next digit when required. Assumes both parameters are character arrays
+pointing to strings with only integer characters. Returns character array of the result
+of the subtraction. 
+*/
 char *subtractString(char *str1, char *str2)
 {
-
+    // Establish req variables. 
     char *result;
     int size;
     char *arrA;
     char *arrB;
-    char charA;
-    char charB;
     int intA;
     int intB;
     int singleValResultInt;
     int borrow;
-    // First we check if the result will be negative
+    // First we check if the result will be negative. Then we add leading zeros 
+    // to the shorter array to make operations easier.
     int neg = 0;
     if (strlen(str1) < strlen(str2) || (strlen(str1) == strlen(str2) && strcmp(str1, str2) < 0))
     {
+        // If neg then str 2 is larger and will decide the basis for memory allocation
         neg = 1;
         arrA = str2;
         size = strlen(arrA);
@@ -127,8 +140,9 @@ char *subtractString(char *str1, char *str2)
             exit(1);
         }
     }
-    else
+    else 
     {
+        // Else str1 is bigger and thus determines the basis for memory allocations
         arrA = str1;
         size = strlen(arrA);
         arrB = addLeadingZeros(str2, size);
@@ -142,69 +156,18 @@ char *subtractString(char *str1, char *str2)
     result[size] = '\0';
     borrow = 0;
 
-    // printf("arrA is %s\n", arrA);
-    // printf("arrB is %s\n", arrB);
     for (int i = size - 1; i >= 0; i--)
     {
-        // printf("Looping --------------------\n");
-        // printf("arrA is %s\n", arrA);
-        // printf("arrB is %s\n", arrB);
-        // printf("i is %d\n", i);
-        charA = arrA[i];
-        charB = arrB[i];
-        intA = charA - '0';
-        intB = charB - '0';
-        // if (borrow == 1)
-        // {
-        //     intA--;
-        // }
-        // printf("before if intA is %d\n", intA);
-        // if (intA < (intB + borrow))
-        // {
-        //     // intA += 10;
-        //     // borrow = 1;
-        //     intA += 10;                                // Borrow from the next digit
-        //     singleValResultInt = intA - intB - borrow; // Perform the subtraction
-        //     borrow = 1;                                // Set borrow for the next iteration
-        // }
-        // else
-        // {
-        //     if (borrow == 1){
-        //         intA --;
-        //     }
-        //     singleValResultInt = intA - intB - borrow;
-        //     borrow = 0; // No need to borrow
-        // }
-        // if (intA < intB)
-        // {
-        //     intA += 10;
-        //     borrow = 1;
-        //     singleValResultInt = intA - intB;
-        // }
-        // else
-        // {
-        //     if (borrow == 1)
-        //     {
-        //         printf("intA decremented by 1");
-        //         intA--;
-        //     }
-        //     if (intA < intB)
-        //     {
-        //         intA += 10;
-        //         borrow = 1;
-        //         singleValResultInt = intA - intB;
-        //     }
-        //     else
-        //     {
-        //         borrow = 0;
-        //     }
-        //     singleValResultInt = intA - intB;
-        // }
+        // For each int in the size of the larger array
 
-                // Adjust intA for any prior borrowing
+        intA = arrA[i] - '0';
+        intB = arrB[i] - '0';
+
+
+        // Adjust intA from any prior borrowing
         if (borrow == 1)
         {
-            intA--;  // Account for the previous borrow
+            intA--;
             borrow = 0;
         }
 
@@ -214,25 +177,15 @@ char *subtractString(char *str1, char *str2)
             intA += 10; // Borrow from the next higher digit
             borrow = 1; // Set borrow for the next iteration
         }
-        // printf("after if intA is %d\n", intA);
-        // printf("intB is %d\n", intB);
-        // printf("borrow is %d\n", borrow);
-
-        // // borrow = 0;
-        singleValResultInt = intA - intB;
-        // printf("singleValResultInt is %d\n", singleValResultInt);
-        // printf("singleValResultInt  + '0' is %c\n", singleValResultInt + '0');
-
+        singleValResultInt = intA - intB; // Calc new digit
         result[i] = singleValResultInt + '0';
-        // printf("result[i] is %c\n", result[i]);
     }
-
     result = removeLeadingZeros(result);
 
     if (neg == 1)
     {
-
-        char *final = malloc(sizeof(char) * strlen(result));
+        // If negative result allocate more memory and add negative sign
+        char *final = malloc(sizeof(char) * (strlen(result)+1));
         if (final == NULL)
         {
             fprintf(stderr, "Error: Out of memory");
@@ -246,19 +199,25 @@ char *subtractString(char *str1, char *str2)
     return result;
 }
 
+/*char *addStrings(char *str1, char *str2) -- Major method for string addition. 
+Calculates the value by iterating through the length of the longer string and conducting
+addition on each corresponding index, starting with the rightmost, or least significant,
+digit. Carries from previous digit when required. Assumes both parameters are character arrays
+pointing to strings with only integer characters. Returns character array of the result
+of the addition. 
+*/
 char *addStrings(char *str1, char *str2)
 {
-
+    // Establish pointers
     char *result;
     int size;
     char *arrA;
     char *arrB;
-    char charA;
-    char charB;
     int intA;
     int intB;
     int singleValResultInt;
-    //  Allocate memory for result depending on large char array size
+    // Allocate memory for result depending on larger char array size
+    // Add leading zeros to shorter array to make operations easier
     if (strlen(str1) > strlen(str2))
     {
         size = strlen(str1);
@@ -285,19 +244,16 @@ char *addStrings(char *str1, char *str2)
         arrA = str2;
         arrB = addLeadingZeros(str1, size);
     }
+
     result[size+1] = '\0';
     int carry = 0;
 
-    // printf("arrA is %s\n", arrA);
-    // printf("arrB is %s\n", arrB);
-
     for (int i = size - 1; i >= 0; i--)
     {
-
-        charA = arrA[i];
-        charB = arrB[i];
-        intA = charA - '0';
-        intB = charB - '0';
+        // For each character in string we get its int value, and calculate the sum of 
+        // both values plus the result of the carry from the previous additions
+        intA = arrA[i]; - '0';
+        intB = arrB[i]; - '0';
 
         singleValResultInt = intA + intB + carry;
         if (singleValResultInt > 9)
@@ -313,7 +269,7 @@ char *addStrings(char *str1, char *str2)
     }
 
 
-
+    // Incase final digit carries
     if (carry == 1)
     {
         result[0] = '1';
@@ -324,6 +280,7 @@ char *addStrings(char *str1, char *str2)
 
 int main()
 {
+    // Establish pointers 
     int errorOccurred = 0;
     char *operation = NULL;
     char *str1 = NULL;
@@ -333,17 +290,16 @@ int main()
     size_t len2 = 0;
     int retVal;
     char *result;
-
+    
+    // Get first line an error under multiple shown conditions
     retVal = getline(&operation, &lenOp, stdin);
     newlineToNullTerm(operation);
-
     if ((strcmp(operation, "add") != 0) && (strcmp(operation, "sub") != 0))
     {
         errorOccurred = 1;
         fprintf(stderr, "Error: 1st line not equal to 'add' or 'sub'\n");
         return errorOccurred;
     }
-    // checkReturnValue(retVal, 1,"Error: 1st line not equal to 'add' or 'sub'\n" );
 
     // Get next line
     // Check if end of line or error reading line
@@ -372,10 +328,8 @@ int main()
 
     str1 = removeLeadingZeros(str1);
     str2 = removeLeadingZeros(str2);
-    // printf("After removing leading zeros str1 is %s \n", str1);
-    // printf("After removing leading zeros str2 is %s \n", str2);
 
-    // If first string zero, return second, covering negative case
+    // If both strings zero, return zero. 
     if ((strcmp(str1, "0") == 0) && (strcmp(str2, "0") == 0))
     {
         printf("%s\n", str1);
@@ -389,7 +343,7 @@ int main()
         return errorOccurred;
     }
 
-
+    // If first string zero, return second string, checking for subtraction for neg sign
     if (strcmp(str1, "0") == 0)
     {
         if (strcmp(operation, "sub") == 0)
@@ -401,10 +355,7 @@ int main()
         return errorOccurred;
     }
 
-
-
-
-
+    // Adding case
     if (strcmp(operation, "add") == 0)
     {
         result = addStrings(str1, str2);
@@ -412,9 +363,8 @@ int main()
         printf("%s\n", result);
     }
     else
-    {
+    { // Subtracting case
         result = subtractString(str1, str2);
-        // removeLeadingZeros(result);
         printf("%s\n", result);
     }
 
