4a9b919e8d0d6192055b26d862315ca974b22018
stevengeorge128 <stevengeorge@arizona.edu>
2024-10-04 17:45:42 -0700

    ettemptedtoimplementaddition

--- strmath.c
+++ strmath.c
@@ -58,14 +58,62 @@ char *removeLeadingZeros(char *str)
 char *addStrings(char *str1, char *str2)
 {
     char *result;
+    int size;
+    int minSize;
+    char *arrA;
+    char *arrB;
+    char charA;
+    char charB;
+    int intA;
+    int intB;
+    int singleValResult;
+    // Allocate memory for result depending on large char array size
     if (strlen(str1) > strlen(str2))
     {
         result = malloc(strlen(str1) + 1);
+        size = strlen(str1);
+        minSize = strlen(str2);
+        arrA = str1;
+        arrB = str2;
     }
     else
     {
         result = malloc(strlen(str2) + 1);
+        size = strlen(str2);
+        minSize = strlen(str1);
+        arrA = str2;
+        arrB = str1;
     }
+
+    int carry = 0;
+    for (int i = size - 1; i >= 0; i--)
+    {
+        // Get each character
+        // Assign zero if outside index of smaller one
+        // subtract '0' from each
+        // Add together.
+        // If grater than 9, then do mod 10 and set to new digit and
+        // set carry to 1
+        // Add this to the result character array
+        charA = arrA[i];
+        if (!(i >= minSize)){
+            charB = arrB[i];
+        } else{
+            charB = '0';
+        }
+        intA = charA - '0';
+        intB = charB - '0';
+        singleValResult = intA + intB + carry;
+        if (singleValResult > 9){
+            singleValResult = singleValResult % 10; 
+            carry = 1;
+        }
+        result[i] = singleValResult;
+
+
+    }
+
+    return result;
 }
 
 int main()
@@ -108,7 +156,7 @@ int main()
     retVal = getline(&str2, &len2, stdin);
     checkReturnValue(retVal, 1, "Error: not enough inputs");
     newlineToNullTerm(str2);
-    if (isArrayNumeric(str1) == 0)
+    if (isArrayNumeric(str2) == 0)
     {
         errorOccurred = 1;
         fprintf(stderr, "Error: Non-integer characters in input");
@@ -117,6 +165,27 @@ int main()
 
     str1 = removeLeadingZeros(str1);
     str2 = removeLeadingZeros(str2);
+    printf("After removing leading zeros str1 is %s \n", str1);
+    printf("After removing leading zeros str2 is %s \n", str2);
+
+    // If first string zero, return second, covering negative case
+    if (strcmp(str1, "0") == 0)
+    {
+        if (strcmp(operation, "sub") == 0)
+        {
+            printf("-%s\n", str2);
+            return errorOccurred;
+        }
+        printf("%s\n", str2);
+        return errorOccurred;
+    }
+
+    // If second string zero, return first string
+    if (strcmp(str2, "0") == 0)
+    {
+        printf("%s\n", str1);
+        return errorOccurred;
+    }
 
     if (strcmp(operation, "add") == 0)
     {
