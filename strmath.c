fa47f36baed677ff25b1bb4130c3f74c4a628ad0
stevengeorge128 <stevengeorge@arizona.edu>
2024-10-04 17:23:40 -0700

    more

--- strmath.c
+++ strmath.c
@@ -1,17 +1,19 @@
 /*
-* File: strmath.c* Author: Steven George
-* Purpose: Read in two strings of unknown length consisting of solely integers
-* perform the given operation on the strings (either addition or subtraction)
-*/
+ * File: strmath.c* Author: Steven George
+ * Purpose: Read in two strings of unknown length consisting of solely integers
+ * perform the given operation on the strings (either addition or subtraction)
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
 
-void checkReturnValue(int val, int returnIfValue, char* message){
-    if (val < returnIfValue){
-        fprintf(stderr,"%s", message);
+void checkReturnValue(int val, int returnIfValue, char *message)
+{
+    if (val < returnIfValue)
+    {
+        fprintf(stderr, "%s", message);
         exit(1);
     }
 }
@@ -24,16 +26,50 @@ void newlineToNullTerm(char *operation)
     }
 }
 
-int isArrayNumeric(char *str){
-    for (int i = 0 ; i < strlen(str); i++){
-        if (!isdigit(str[i])){
+int isArrayNumeric(char *str)
+{
+    for (int i = 0; i < strlen(str); i++)
+    {
+        if (!isdigit(str[i]))
+        {
             return 0;
         }
     }
     return 1;
 }
 
-int main() {
+char *removeLeadingZeros(char *str)
+{
+    // int len = strlen(str);
+    // int breakPoint = -1;
+    // for (int i = 0; i < len; i ++){
+    //     if (str[i] != '0'){
+    //         return str[i];
+    //     }
+    // }
+    // While first character is zero and we haven't reached the null terminator
+    while (*str == '0' && *(str + 1) != '\0')
+    {
+        str++;
+    }
+    return str;
+}
+
+char *addStrings(char *str1, char *str2)
+{
+    char *result;
+    if (strlen(str1) > strlen(str2))
+    {
+        result = malloc(strlen(str1) + 1);
+    }
+    else
+    {
+        result = malloc(strlen(str2) + 1);
+    }
+}
+
+int main()
+{
     int errorOccurred = 0;
     char *operation = NULL;
     char *str1 = NULL;
@@ -42,46 +78,50 @@ int main() {
     size_t len1 = 0;
     size_t len2 = 0;
     int retVal;
-    
-    
+
     retVal = getline(&operation, &lenOp, stdin);
     newlineToNullTerm(operation);
 
-    if ((strcmp(operation, "add") != 0) && (strcmp(operation, "sub") != 0)){
+    if ((strcmp(operation, "add") != 0) && (strcmp(operation, "sub") != 0))
+    {
         errorOccurred = 1;
-        fprintf(stderr,"Error: 1st line not equal to 'add' or 'sub'");
+        fprintf(stderr, "Error: 1st line not equal to 'add' or 'sub'");
         return errorOccurred;
     }
-    //checkReturnValue(retVal, 1,"Error: 1st line not equal to 'add' or 'sub'" );
+    // checkReturnValue(retVal, 1,"Error: 1st line not equal to 'add' or 'sub'" );
 
+    // Get next line
+    // Check if end of line or error reading line
+    // Convert newline character to null terminator
+    // Ensure newline is numeric
     retVal = getline(&str1, &len1, stdin);
-    checkReturnValue(retVal, 1,"Error: not enough inputs" );
+    checkReturnValue(retVal, 1, "Error: not enough inputs");
     newlineToNullTerm(str1);
-    if (isArrayNumeric(str1) == 0){
+    if (isArrayNumeric(str1) == 0)
+    {
         errorOccurred = 1;
-        fprintf(stderr,"Error: Non-integer characters in input");
+        fprintf(stderr, "Error: Non-integer characters in input");
         return errorOccurred;
     }
 
+    // Repeat for second string int
     retVal = getline(&str2, &len2, stdin);
-    checkReturnValue(retVal, 1,"Error: not enough inputs" );
+    checkReturnValue(retVal, 1, "Error: not enough inputs");
     newlineToNullTerm(str2);
-    if (isArrayNumeric(str1) == 0){
+    if (isArrayNumeric(str1) == 0)
+    {
         errorOccurred = 1;
-        fprintf(stderr,"Error: Non-integer characters in input");
+        fprintf(stderr, "Error: Non-integer characters in input");
         return errorOccurred;
     }
-    
-    newlineToNullTerm(str1);
-    newlineToNullTerm(str2);
 
+    str1 = removeLeadingZeros(str1);
+    str2 = removeLeadingZeros(str2);
 
-    
-    
-    // while (getline(&, &len, stdin) >= 0){
-    //     printf("Line: %s", line);
-    // }
+    if (strcmp(operation, "add") == 0)
+    {
+        addStrings(str1, str2);
+    }
 
- 
     return errorOccurred;
 }
